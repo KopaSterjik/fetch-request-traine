3768f72ff2bb81d5d92545c862ed56ff281e03ad
Kopa Sterjik <linegod8@gmail.com>
2025-10-19 14:21:41 +0300

    finally

--- traine.js
+++ traine.js
@@ -22,3 +22,39 @@ loadPostFormElement.addEventListener("submit", (event) => {
       resultElement.innerHTML = error.message;
     });
 });
+
+const createPostFormElement = document.querySelector(".create-post-form");
+
+createPostFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(createPostFormElement);
+  const formDataObject = Object.fromEntries(formData);
+  fetch("http://localhost:3000/posts", {
+    method: "post",
+    body: JSON.stringify({ ...formDataObject, views: 0 }),
+  })
+    .then((response) => {
+      console.log("response: ", response);
+      return response.json();
+    })
+    .then((json) => {
+      console.log("json: ", json);
+    });
+});
+
+const searchPostsFormElement = document.querySelector(".search-posts-form");
+const postViewsInputElement = document.querySelector("#post-views");
+
+searchPostsFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  fetch(`http://localhost:3000/posts?views_gte=${postViewsInputElement.value}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
+      resultElement.innerHTML = json
+        .map(({ title, views }) => `<p>${title}, просмотров: ${views}</p>`)
+        .join("");
+    });
+});
